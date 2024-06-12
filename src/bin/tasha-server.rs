a6a0b210f1dbfff4703a2ac2736c7ebbfd9a44ca
Xu Shaohua <shaohua@biofan.org>
2024-06-13 00:47:35 +0800

    Use jemalloc

--- src/bin/tasha-server.rs
+++ src/bin/tasha-server.rs
@@ -2,8 +2,13 @@
 // Use of this source is governed by GNU Affero General Public License
 // that can be found in the LICENSE file.
 
+use tikv_jemallocator::Jemalloc;
+
 use tasha::error::Error;
 use tasha::server;
+
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
 
 fn main() -> Result<(), Error> {
     server::run::handle_cmdline()
