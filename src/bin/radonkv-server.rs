7cc47170e1ad448b8e9d9685122118364cd4dab7
Xu Shaohua <shaohua@biofan.org>
2024-07-09 22:43:00 +0800

    Make jemalloc optional

--- src/bin/radonkv-server.rs
+++ src/bin/radonkv-server.rs
@@ -2,13 +2,12 @@
 // Use of this source is governed by GNU Affero General Public License
 // that can be found in the LICENSE file.
 
-use tikv_jemallocator::Jemalloc;
-
 use radonkv::error::Error;
 use radonkv::server;
 
+#[cfg(feature = "jemalloc")]
 #[global_allocator]
-static GLOBAL: Jemalloc = Jemalloc;
+static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
 
 fn main() -> Result<(), Error> {
     server::run::handle_cmdline()
